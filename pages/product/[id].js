08be27f6e62bdb6c4fc0e62b99fc7ec9195b6e6b
Masoud bahamin <Bahaminwp@gmail.com>
2023-11-30 12:03:08 +0300

    test

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -12,38 +12,43 @@ export default function Product({ data }) {
         <div>
             <BreadCrumb title={"Product Details"} />
             <ProductDisplay {...data} />
-            {/* <Comments _id={data._id} comments={data.comments}/> */}
+            <Comments _id={data._id} comments={data.comments} />
         </div>
     )
 }
 
 
-export async function getStaticPaths() {
-    connectToDb()
-    const allproducts = await productModel.find({})
-    const products = JSON.parse(JSON.stringify(allproducts))
+// export async function getStaticPaths() {
+//     connectToDb()
+//     const allproducts = await productModel.find({})
+//     const products = JSON.parse(JSON.stringify(allproducts))
 
-    const paths = products.map(product => ({ params: { id: product._id } }))
-    return {
-        paths ,
-        fallback: true
-    }
-}
+//     const paths = products.map(product => ({ params: { id: product._id } }))
+//     return {
+//         paths ,
+//         fallback: true
+//     }
+// }
 
-export async function getStaticProps(context) {
+// export async function getStaticProps(context) {
+//     connectToDb()
+//     const { id } = context.params
+//     const data = await productModel.findOne({ _id: id }).populate("comments").lean()
+//     if (!data) {
+//         return {
+//             redirect: { destination: "/" }
+//         }
+//     }
+//     return {
+//         props: { data: JSON.parse(JSON.stringify(data)) },
+//         revalidate: 300
+//     }
+// }
+
+export async function getServerSideProps(context) {
     connectToDb()
-    const { id } = context.params
-    const data = await productModel.findOne({ _id: id }).populate("comments").lean()
+
+    const product = await productModel.findOne({ _id: context.params.id })
     
-    if (!data) {
-        return {
-            redirect: { destination: "/" }
-        }
-    }
-    return {
-        props: { data: JSON.parse(JSON.stringify(data)) },
-        revalidate: 300
-    }
+    return { props: { data: JSON.parse(JSON.stringify(product)) } }
 }
-
-
